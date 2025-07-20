4c5466f158cef68570d2e35020632bf0a8a3a263
HafizhHabiibi <hafizhhabiibi123@gmail.com>
2025-07-20 19:30:49 +0700

    fix(config): URI to URL

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ import os
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = '123'
     SESSION_TYPE = 'filesystem'
